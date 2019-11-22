34f19b506cca650ad9e45bb8506e5c7a18177aac
Matt Seto <seanseto@gmail.com>
2019-11-22 12:11:39 -0800

    Remove site folder

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\DOORM4T\\Documents\\GitHub\\benefits-of-birding\\.cache\\dev-404-page.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\DOORM4T\\Documents\\GitHub\\benefits-of-birding\\src\\pages\\index.js")))
+}
+
